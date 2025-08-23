10d123e0e104ed3c58271122f3b690f9046d81db
Murilo Ribeiro <murilo@fetch.ly>
2025-08-23 13:16:35 -0300

    Debug CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,15 +5,18 @@ import { config } from './config';
 
 export async function createServer(): Promise<Application> {
     const app = express();
+
+    console.log(`[DEBUG] process.env.CORS_ORIGIN: ${process.env.CORS_ORIGIN}`);
+    console.log(`[DEBUG] config.corsOrigin: ${config.corsOrigin}`);
     
     app.use(cors({
-        origin: config.corsOrigin, 
+        origin: config.corsOrigin,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         optionsSuccessStatus: 204
     }));
 
     app.use(express.json());
-    
+
     const usuarioController = (await import('./controller/UsuarioController')).default;
     const locadorController = (await import('./controller/LocadorController')).default;
     const locatarioController = (await import('./controller/LocatarioController')).default;
@@ -21,13 +24,13 @@ export async function createServer(): Promise<Application> {
     app.use('/api', usuarioController);
     app.use('/api/locador', locadorController);
     app.use('/api/locatario', locatarioController);
-    
+
     app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
         if (err instanceof HttpError) {
             res.status(err.statusCode).json({ message: err.message });
             return;
         }
-        console.error("Erro não tratado:", err); 
+        console.error("Erro não tratado:", err);
         res.status(500).json({ message: 'Erro interno do servidor.' });
     });
 
